e0b17dbeef13cc075a4433b35eeea5dfc7596b73
Lebedev-023046 <lebedev.023046@gmail.com>
2022-01-24 22:32:14 +0300

    feat: add adaptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,22 @@
 const hamburger = document.querySelector(".hamburger")
+const navigation = document.querySelector(".nav-list")
 
 const toggleMenu = () => {
-    hamburger.classList.toggle("open")
+    hamburger.classList.toggle("open-icon")
+    navigation.classList.toggle("open")
 }
 
 hamburger.addEventListener("click", toggleMenu)
+
+function closeMenu(event) {
+    if (event.target.classList.contains('nav-link')) {
+      hamburger.classList.remove("open-icon");
+      navigation.classList.remove("open");
+    }
+  }
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach((el) => el.addEventListener('click', closeMenu));
+
+
+console.log('Всего баллов: 75 из 75.\nВёрстка соответствует макету. Ширина экрана 768px +48\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15\nНа ширине экрана 768рх и меньше реализовано адаптивное меню +22')
