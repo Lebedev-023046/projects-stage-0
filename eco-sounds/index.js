b7acefcad891546d0f4ca0c68157b616dfdd1712
Lebedev-023046 <lebedev.023046@gmail.com>
2022-02-03 14:34:06 +0300

    feat: add all functionality

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,20 +1,55 @@
+let isPlay = false
+
 // ACTIVEBUTTON
 
 const birdList = document.querySelector(".birds-list")
 const birdItemList = document.querySelectorAll(".bird-item")
 const mainContainer = document.querySelector(".main")
 
+const audio = document.querySelector(".audio-block")
+
+
+// PLAYPAUSESWITCH
+
+const playBtn = document.querySelector(".play")
+
+const changeCondition = () => {
+    playBtn.classList.toggle("pause")
+    if ((playBtn.classList.contains("pause"))) {
+        isPlay = true
+    }else{
+        isPlay = false
+    }
+}
+
+playBtn.addEventListener("click", changeCondition)
+
+
+// PLAYFUNCTION
+
+const playAudio = () => {
+    if (!(isPlay)) {
+        audio.pause()
+    }else{
+        audio.play()
+    } 
+}
+
+playBtn.addEventListener("click", playAudio)
+
 const activeButton = (event) => {
+    playBtn.classList.remove("pause")
     if (event.target.classList.contains("bird-item")) {
         birdItemList.forEach(element => {
             element.classList.remove("bird-active")
         })
         event.target.classList.add("bird-active")
-        console.log(event.target.dataset.birdname)
         mainContainer.style.backgroundImage = `url(assets/img/${event.target.dataset.birdname}.jpg)`
+        audio.src = `assets/audio/${event.target.dataset.birdname}.mp3`
     }
 }
 
 birdList.addEventListener("click", activeButton)
+
 
 
