e65103054ee94f54d0ae741a14a8cb450a7f2e1c
Lebedev-023046 <lebedev.023046@gmail.com>
2022-02-10 01:28:02 +0300

    feat: add some functionality

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,8 +1,40 @@
-const url = 'https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const galeryContainer = document.querySelector(".container")
 
-async function getData() {
+function createElement(imgData) {
+    const div = document.createElement("div")
+    div.classList.add("img")
+    div.style.backgroundImage = `url(${imgData})`;
+    galeryContainer.append(div)
+}
+
+const cancelBtn = document.querySelector(".cancel-button")
+
+async function getData(url) {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+
+    data.results.forEach(element => {
+        createElement(element.urls.regular)
+    }); 
   }
-  getData();
+
+const searchBtn = document.querySelector(".search-button")
+const input = document.querySelector(".search-input")
+
+const workflow = () => {
+    const inputValue = document.querySelector(".search-input").value
+    const url = `https://api.unsplash.com/search/photos?query=${inputValue}&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
+    getData(url)
+}
+
+searchBtn.addEventListener("click", workflow)
+input.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+        workflow()
+    }
+})
+
+cancelBtn.addEventListener("click", () => {
+    input.value = ''
+})
+
